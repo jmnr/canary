30a16ed0e20f87984345f9a5974f236f8459056e
wallcrawler <ronan_mccabe@hotmail.com>
2015-06-01 16:08:55 +0100

    div add

--- script.js
+++ script.js
@@ -2,17 +2,11 @@
   "use strict";
 
   $('#submitButton').click(function() {
-    $.post( '/addClap', $('#newClapInput').val(), function(data){
-      console.log(data);
-    });
+    $.post( '/addClap', $('#newClapInput').val(), addClap(data) );
   });
 
-//   $.post( "test.php", { func: "getNameAndTime" }, function( data ) {
-//   console.log( data.name ); // John
-//   console.log( data.time ); // 2pm
-// }, "json");
-  // var addClap = function() {
-  //   alert('posted');
-  // };
+  var addClap = function() {
+    
+  };
 
 }());
