e1190be7fe52aed0b85f97afdcd329a9ded65304
Nikhila Ravi <nikkiravi92@gmail.com>
2015-06-01 17:30:24 +0100

    writing tweets to a file

--- handlers.js
+++ handlers.js
@@ -1,20 +1,29 @@
 var handlers = {};
-var clapList = [];
 var fs = require('fs');
 
 handlers['POST /addClap'] = function(req, res) {
-
   req.on('error', function(err) {
    console.log('problem with request: ' + err.message);
    });
 
-  req.on('data', function(chunk) {
-     clapList.push(chunk.toString());
+   var body = '';
+
+   req.on('data', function(chunk) {
+      body += chunk;
    });
 
   req.on('end', function() {
+
+    var entry = JSON.stringify({message: body}) + "\n";
+    console.log(entry);
+
+    fs.appendFile('claps.json', entry, function (err) {
+      if (err) throw err;
+      console.log('The "data to append" was appended to file!');
+    });
+
      res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-     res.end(clapList[(clapList.length)-1]);
+     res.end(body);
    });
 };
 
--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 
   $('#submitButton').click(function() {
     $.post( '/addClap', $('#newClapInput').val(), function(data) {
+      data = JSON.parse(data.slice(0, data.length-3));
+      console.log(data);
       $("#claps").append(
-        '<div class="clap">' + data + '</div>'
+        '<div class="clap">' + data.message + '</div>'
       );
     });
   });
-
-
 
 }());
