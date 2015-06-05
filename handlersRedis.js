dbb4457adfa5f0b7c65780d4f3433902409c9105
Nikhila Ravi <nikkiravi92@gmail.com>
2015-06-05 17:16:39 +0100

    deleted favicon

--- handlersRedis.js
+++ handlersRedis.js
@@ -87,14 +87,14 @@ handlers['POST /delete'] = function(req, res) {
 
 handlers.generic = function(req, res) {
   fs.readFile(__dirname + req.url, function(err, data){
-    // if (err){
-    //   res.end();
-    // }
-    // else {
+    if (err){
+      res.end();
+    }
+    else {
       var ext = req.url.split('.')[1];
       res.writeHead(200, {'Content-Type' : 'text/' + ext});
       res.end(data);
-    // }
+    }
   });
 };
 
