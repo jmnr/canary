61787b109550dbc9d5ae91bedbf32d418a8da815
Nikhila Ravi <nikkiravi92@gmail.com>
2015-06-05 17:25:31 +0100

    changed port

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var handlers = require('./handlersRedis.js');
 // var handlers = require('./handlers.js');
 var serverHandler = require('./serverHandler.js');
 
-http.createServer(serverHandler).listen(8000);
+http.createServer(serverHandler).listen(process.env.PORT ||8000);
