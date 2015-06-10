26d7086ef3964c6b3a7689e834805838bd97eac5
wallcrawler <ronan_mccabe@hotmail.com>
2015-06-10 17:40:41 +0100

    ok heroku

--- redisCrd.js
+++ redisCrd.js
@@ -41,7 +41,7 @@ var redisCrd = {
     client.scan(0, function(err, data) {
       var claps = data[1];
       len = claps.length;
-      for(var i = len - 1; i >= 0; i--) {
+      for(var i = 0; i < len; i--) {
         client.hgetall(claps[i], callback);
       }
     });
