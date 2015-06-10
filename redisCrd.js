e8f8c8e3c81e51c5e4114529033e230fe1e47792
wallcrawler <ronan_mccabe@hotmail.com>
2015-06-10 17:17:44 +0100

    working

--- redisCrd.js
+++ redisCrd.js
@@ -2,71 +2,56 @@ var redis = require("redis");
 var geolocation = require("./geolocation.js");
 
 // //local
-// var client = redis.createClient();
+var client = redis.createClient();
 // //local
 
 //heroku
-var url = require('url');
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
-var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-client.auth(redisURL.auth.split(":")[1]);
+// var url = require('url');
+// var redisURL = url.parse(process.env.REDISCLOUD_URL);
+// var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+// client.auth(redisURL.auth.split(":")[1]);
 //heroku
 
 var redisCrd = {
-  write: function(clap, res) {
+  create: function(clap, res) {
     var clapObj = {
-            userId: clap.cookie,
-            message: clap.message,
-            time: new Date().getTime(),
-            lat: clap.lat,
-            lon: clap.lon
-            };
-    console.log("clapObj:",clapObj);
+      userId: clap.cookie,
+      message: clap.message,
+      time: new Date().getTime(),
+      lat: clap.lat,
+      lon: clap.lon
+    };
     client.hmset(clapObj.time, clapObj, function(err){
       res.writeHead(200, "OK", {'Content-Type': 'text/html'});
       res.end(JSON.stringify(clapObj)); //sends back new tweet for display
     });
-    client.sadd("tweets", clapObj.time);
   },
 
-  readAll: function(res) {
-    var responses = [];
-    client.sort("tweets", function(err, data) {
-      var tweetnumber = data.length;
-      for (var i=(tweetnumber-1); i>=0; i--) {
-        client.hgetall(data[i], function(err, obj) {
-          responses.push(obj);
-          if (responses.length === tweetnumber) {
-            res.end(JSON.stringify(responses));
-          }
-        });
+  read: function(res) {
+    var clapsLoad = [];
+    var len;
+
+    var callback = function(err, data) {
+      clapsLoad.push(data);
+      if(clapsLoad.length === len) {
+        res.end(JSON.stringify(clapsLoad));
+      }
+    };
+
+    client.scan(0, function(err, data) {
+      var claps = data[1];
+      len = claps.length;
+      for(var i = len - 1; i >= 0; i--) {
+        client.hgetall(claps[i], callback);
       }
     });
   },
 
-  remove: function(time, res) {
-    client.srem("tweets", -1, time);
+  delete: function(time, res) {
+    client.del(time);
     res.end();
   }
 
 };
 
 module.exports = redisCrd;
-
-// client.get("tweetcount", function(err, reply) {
-//   var tweetnumber = Number(reply);
-//   console.log("tweetcount: ", tweetnumber);
-//   for (var i = tweetnumber; i > 0; i--) {
-//     console.log("i:", i);
-//     client.hgetall(i, function(err, obj) {
-//       console.log("obj:",obj);
-//       responses.push(obj);
-//       if (responses.length === tweetnumber) {
-//         console.log("Responses:", responses);
-//         // res.end(JSON.stringify([{time: 1, userId: 1234567890, message: "hello"}]));
-//           res.end(JSON.stringify(responses));
-//         }
-//       })
-//     }
-//
-//   })
