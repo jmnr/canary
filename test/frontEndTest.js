5cc575124722722838d29926a575e5d9162d2e96
msmichellegar <msmichellegar@gmail.com>
2015-06-10 16:27:57 +0100

    added tab functionality

--- test/frontEndTest.js
+++ test/frontEndTest.js
@@ -2,3 +2,7 @@ test("check CSS is loading properly", function() {
   var content = getComputedStyle(document.getElementById('content'), null).margin;
   equal(content, '50px 100px', 'css is present');
 });
+
+test("check carousel switches properly", function() {
+  equal('clicking tab function', 'display: none', 'tabs function correctly');
+});
