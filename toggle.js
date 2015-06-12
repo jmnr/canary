4c2373bad6530c34348b8973f8230ea1a1ed1069
msmichellegar <msmichellegar@gmail.com>
2015-06-12 14:47:57 +0100

    toggle slide css

--- toggle.js
+++ toggle.js
@@ -1,9 +1,19 @@
 function toggleSlide (num) {
   var elements = document.getElementsByClassName("tabs");
+  var toMap = document.getElementById("toMap");
+  var toFeed = document.getElementById("toFeed");
   elements[num].style.display = "none";
   if (num === 0) {
     elements[1].style.display = "block";
+    toMap.style.background = "#ffff7e";
+    toMap.style.color = "black";
+    toFeed.style.background = "black";
+    toFeed.style.color = "white";
   } else {
     elements[0].style.display = "block";
+    toFeed.style.background = "#ffff7e";
+    toFeed.style.color = "black";
+    toMap.style.background = "black";
+    toMap.style.color = "white";
   }
 }
